74882ea8e015cb65413c10e30c6cce7ebaa0032b
capenna <alessiocaperna@gmail.com>
2020-09-20 17:00:10 +0200

    bug-fixes and footer

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,10 @@ async function carousel(){
   let par = document.querySelector('.qualities');
   let i = 0;
   setInterval(function(){
-    if (i === qualities.length -1)
+    if (i === qualities.length - 1){
+      par.textContent = qualities[i]; 
       i = 0;
+    }
     else
       par.textContent = qualities[i++];
 
